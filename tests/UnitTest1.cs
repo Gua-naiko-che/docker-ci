d07b0d487f7c10e12ee9095d4e52b94a1be5f343
Tao Gómez Gil <tao.gomez.gil@gmail.com>
2019-11-20 19:32:50 +0100

    Create real app Selenium test

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,6 +1,6 @@
-using System;
 using System.IO;
 using System.Reflection;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Xunit;
 
@@ -11,10 +11,11 @@ namespace tests
         [Fact]
         public void Test1()
         {
-            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
-            {
-                driver.Navigate().GoToUrl("https://google.com");
-            }
+            using var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:5001/");
+            By titleSelector = By.CssSelector("h1");
+            IWebElement title = driver.FindElement(titleSelector);
+            Assert.Equal("Hello, world!", title.Text);
         }
     }
 }
